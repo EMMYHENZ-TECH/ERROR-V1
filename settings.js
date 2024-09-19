f0124c695c53325ecbb0411132747b4bd3fef9f2
EMMY_HENZ-TECH <emmanueleffangahenshaw072@gmail.com>
2024-09-19 02:11:09 +0100

    Add files via upload

--- settings.js
+++ settings.js
@@ -16,14 +16,17 @@ global.websitex = "https://www.youtube.com/@EMMYHENZTECHINFO"
 global.wagc = "https://whatsapp.com/channel/0029VangYOt96H4JhFarL10C"
 global.themeemoji = '🪀'
 global.wm = "Xeon Bot Inc."
-global.botscript = 'https://github.com/EMMYHENZ-TECH/ERROR-V1' //script link
-global.packname = "EMMY HENZ"
+global.botscript = 'https://github.com/' //script link
+global.packname = "Sticker By"
 global.author = "EMMYTECHINFO\n\n+2349121785627"
 global.creator = "2349125042727@s.whatsapp.net"
 global.xprefix = '.'
 global.premium = ["2349125042727"] // Premium User
 
-global.link = 'https://whatsapp.com/channel/0029VangYOt96H4JhFarL10C'
+//channel id
+global.xchannel = {
+	jid: '120363319098372999@newsletter'
+	}
 
 //bot sett
 global.typemenu = 'v12' // menu type 'v1' => 'v12'
